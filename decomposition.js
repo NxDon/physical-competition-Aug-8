86721ebed28d2cfa18cc91ac74394186c98a7546
NxDon <645104928@qq.com>
2017-08-08 15:36:47 +0800

    change code without prime number check

--- decomposition.js
+++ decomposition.js
@@ -1,38 +1,39 @@
 function decomposition(num) {
-    var tempnum = num;
-    primefactors = [];//质因数
+    var originNum = num;
+    primeFactors = [];//质因数
     var n = 0;
     while (num >= n) {
         if (n == num) {
-            primefactors.push(n);
+            primeFactors.push(n);
             break;
         }
-        if (num % n === 0 && isPrime(n)) {//可以整除
+        if (num % n === 0 && n >= 2) {//可以整除
             num = num / n;
-            primefactors.push(n);
+            primeFactors.push(n);
         } else {//不能整除，n++
             n++;
         }
     }
-    return tempnum+"="+primefactors.join("");
+    return originNum + "=" + primeFactors.join("");
 }
 
-function isPrime(num) {
-    if (num < 2) {
-        return false
-    }
-    if (num === 2) {
-        return true
-    } else if (num % 2 === 0) {
-        return false;
-    }
-    var max = Math.sqrt(num);
-    for (var i = 3; i <= max; i += 2) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-console.log(decomposition(90));
+//
+// function isPrime(num) {
+//     if (num < 2) {
+//         return false
+//     }
+//     if (num === 2) {
+//         return true
+//     } else if (num % 2 === 0) {
+//         return false;
+//     }
+//     var max = Math.sqrt(num);
+//     for (var i = 3; i <= max; i += 2) {
+//         if (num % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+//
+// console.log(decomposition(90));
