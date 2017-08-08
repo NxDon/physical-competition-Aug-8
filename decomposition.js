305f3bcfe4f81a844985a6bab547573887422be7
NxDon <nxduanyu@gmail.com>
2017-08-08 14:43:25 +0800

    add README and comments

--- decomposition.js
+++ decomposition.js
@@ -1,35 +1,36 @@
+/**
+ * Prime factor decomposition
+ * @param {Number} num
+ * @returns {string}
+ */
 function decomposition(num) {
-    var tempnum = num;
-    primefactors = [];//质因数
+    var originNum = num;
+    var primeFactors = [];//质因数数组
     var n = 0;
     while (num >= n) {
-        if (n == num) {
-            primefactors.push(n);
+        if (n === num) {
+            primeFactors.push(n);
             break;
         }
         if (num % n === 0 && isPrime(n)) {//可以整除
             num = num / n;
-            primefactors.push(n);
+            primeFactors.push(n);
         } else {//不能整除，n++
             n++;
         }
     }
-    return tempnum+"="+primefactors.join("");
+    return originNum + "=" + primeFactors.join("");
 }
 
 function isPrime(num) {
-    if (num < 2) {
-        return false
-    }
-    if (num === 2) {
-        return true
-    } else if (num % 2 === 0) {
-        return false;
-    }
-    var max = Math.sqrt(num);
-    for (var i = 3; i <= max; i += 2) {
-        if (num % i === 0) {
+    if (num !== 2) {
+        if (num < 2 || num % 2 === 0) {
             return false;
+        }
+        for (var i = 3; (i * i) <= num; i += 2) {//因为x = a * b,那么一定是a == b 或者 a != b,因此只需循环到sqrt(num)
+            if (num % 2 === 0) {
+                return false;
+            }
         }
     }
     return true;
